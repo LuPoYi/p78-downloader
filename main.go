426e83c14722d2c7b22f7d2d50f5d7477cc48273
Bob <maydaybob2000@gmail.com>
2016-09-22 21:01:59 +0800

    Use FHD Page1~2 For Test

--- main.go
+++ main.go
@@ -1,12 +1,3 @@
-/*
-圖片resp:
-&{200 OK 200 HTTP/1.1 1 1 map[Content-Length:[5607] Last-Modified:[Tue, 08 Mar 2016 05:51:31 GMT] Connection:[keep-alive] Etag:["56de6863-15e7"] Accept-Ranges:[bytes] Date:[Mon, 19 Sep 2016 11:15:40 GMT] Content-Type:[image/png]] 0xc4200146c0 5607 [] false false map[] 0xc4200ce0f0 0xc4203b5600}
-
-網頁resp:
-&{200 OK 200 HTTP/1.1 1 1 map[Content-Type:[text/html; charset=utf-8] X-Request-Id:[98dc0c16-a642-411c-93d3-6b859df5f7be] Etag:[W/"b757cf7e417b9534dea7329f3d5adeee"] Cache-Control:[max-age=0, private, must-revalidate] X-Content-Type-Options:[nosniff] Date:[Mon, 19 Sep 2016 11:16:05 GMT] Connection:[keep-alive] Vary:[Accept-Encoding] Set-Cookie:[BitoEXse2=04ad3605afd1482d57386a159b95a710; path=/; expires=Wed, 21 Sep 2016 11:16:05 -0000; HttpOnly] Status:[200 OK] X-Xss-Protection:[1; mode=block] X-Frame-Options:[SAMEORIGIN]] 0xc420438140 -1 [chunked] false true map[] 0xc4200ee0f0 0xc4202a2000}
- req.Header.Set("name", "value")
-*/
-
 package main
 
 import (
@@ -15,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv" // convert string to int
 
 	"github.com/PuerkitoBio/goquery" // A little like that j-thing, only in Go.
 )
@@ -27,7 +19,7 @@ func goDownload(url, filename string) {
 	}
 	defer resp.Body.Close()
 
-	f, err := os.Create(filename + ".jpg")
+	f, err := os.Create("images/" + filename + ".jpg")
 	if err != nil {
 		panic(err)
 	}
@@ -41,13 +33,12 @@ func goCrawler(url string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Go crawler!")
 
 	doc.Find("ul#portfolio li").Each(func(i int, s *goquery.Selection) {
 		title, _ := s.Find(".picture_overlay img").Eq(0).Attr("alt")
     img_url, _ := s.Find(".picture_overlay img").Eq(0).Attr("src")
 
-    if img_url[0:3] != "http" {
+    if img_url[0:4] != "http" {
       img_url = "https://porn77.info/" + img_url
     }
 
@@ -56,5 +47,11 @@ func goCrawler(url string) {
 }
 
 func main() {
-	goCrawler("https://porn77.info/")
+	for i := 1 ; i < 3 ; i++ {
+		fmt.Println("goCrawler FHD Page " + strconv.Itoa(i))
+		goCrawler("https://porn77.info/video/index/search/FHD/page/" + strconv.Itoa(i))
+	}
 }
+
+
+// http://studygolang.com/articles/8359
